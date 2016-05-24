662dec5b9ec48b32a572d1379497693bd4e9bc42
Dongen, Thomas T.G.T.A. van <tgta.vandongen@student.fontys.nl>
2016-05-24 12:35:50 +0200

    Aanmaken van het project.

---
+++ Spotify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Spotify.Startup))]
+namespace Spotify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
