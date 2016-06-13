df998ebfefd00260b22c6eee8870722bc41b7ff4
Dongen, Thomas T.G.T.A. van <tgta.vandongen@student.fontys.nl>
2016-06-13 05:03:07 +0200

    Unit tests

---
+++ Spotify.Tests/Controllers/AlbumControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spotify.Controllers;
+
+namespace Spotify.Tests.Controllers
+{
+    [TestClass]
+    public class AlbumControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AlbumController controller = new AlbumController();
+            ViewResult index = controller.Index(1) as ViewResult;
+            Assert.IsNotNull(index);
+        }
+    }
+}
---
+++ Spotify.Tests/Controllers/PlaylistControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spotify.Controllers;
+
+namespace Spotify.Tests.Controllers
+{
+    [TestClass]
+    public class PlaylistControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PlaylistController controller = new PlaylistController();
+            ViewResult addsong = controller.AddSong(5,1) as ViewResult;
+            Assert.IsNotNull(addsong);
+        }
+    }
+}
